d42115573052a32fa01d27a9914c96f11084298a
Ray-zero1 <54015319+Ray-zero1@users.noreply.github.com>
2021-07-04 11:54:48 +0900

    Create twelite.py

---
+++ twelite.py
@@ -0,0 +1,84 @@
+from machine import Pin, UART, I2C
+
+#ライブラリのインポート
+import utime, time
+
+#ピン設定
+gps_module = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+uart = UART(1,baudrate=9600,tx=Pin(4),rx=Pin(5))
+#print gps module connection details
+print(gps_module)
+
+#Used to Store NMEA Sentences
+buff = bytearray(255)
+
+TIMEOUT = False
+
+#store the status of satellite is fixed or not
+FIX_STATUS = False
+
+#Store GPS Coordinates
+latitude = ""
+longitude = ""
+satellites = ""
+gpsTime = ""
+
+
+#function to get gps Coordinates
+def getPositionData(gps_module):
+    global FIX_STATUS, TIMEOUT, latitude, longitude, satellites, gpsTime
+    
+    #run while loop to get gps data
+    #or terminate while loop after 5 seconds timeout
+    timeout = time.time() + 8   # 8 seconds from now
+    while True:
+        gps_module.readline()
+        buff = str(gps_module.readline())
+        #parse $GPGGA term
+        #b'$GPGGA,094840.000,2941.8543,N,07232.5745,E,1,09,0.9,102.1,M,0.0,M,,*6C\r\n'
+        #print(buff)
+        parts = buff.split(',')
+        
+        #if no gps displayed remove "and len(parts) == 15" from below if condition
+        if (parts[0] == "b'$GPGGA" and len(parts) == 15):
+            if(parts[1] and parts[2] and parts[3] and parts[4] and parts[5] and parts[6] and parts[7]):
+                
+                latitude = convertToDigree(parts[2])
+                # parts[3] contain 'N' or 'S'
+                longitude = convertToDigree(parts[4])
+                # parts[5] contain 'E' or 'W'
+                satellites = parts[7]
+                gpsTime = parts[1][0:2] + ":" + parts[1][2:4] + ":" + parts[1][4:6]
+                FIX_STATUS = True
+                break
+                
+        if (time.time() > timeout):
+            TIMEOUT = True
+            break
+        utime.sleep_ms(500)
+        
+#function to convert raw Latitude and Longitude
+#to actual Latitude and Longitude
+def convertToDigree(RawDegrees):
+
+    RawAsFloat = float(RawDegrees)
+    firstdigits = int(RawAsFloat/100) #degrees
+    nexttwodigits = RawAsFloat - float(firstdigits*100) #minutes
+    
+    Converted = float(firstdigits + nexttwodigits/60.0)
+    Converted = '{0:.6f}'.format(Converted) # to 6 decimal places
+    return str(Converted)
+    
+    
+while True:
+    
+    getPositionData(gps_module)
+    print("Time:"+str(gpsTime))
+    print("Latitude:"+ str(latitude))
+    print("Longitude:"+str(longitude))
+    uart.write(str(gpsTime))
+    uart.write(str(latitude))
+    uart.write(str(longitude))   
+    if(TIMEOUT == True):
+        print("Request Timeout: No GPS data is found.")
+        TIMEOUT = False
