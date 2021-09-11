25236a15ac912e4eb7bb745f220aa4612dcec070
Ray-zero1 <54015319+Ray-zero1@users.noreply.github.com>
2021-09-11 13:48:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-import _thread
-from machine import Pin, UART, I2C, ADC, Timer
+from machine import Pin, UART, I2C, ADC, Timer, SPI
 import utime, time
 from mpu6050 import MPU6050
+import os
+import sdcard
+from ina219 import INA219
+from logging import INFO
+SHUNT_OHMS = 0.1
 
+i2c = I2C(0,scl=Pin(9),sda=Pin(8))
 gps_module = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 uart = UART(1,baudrate=115200,tx=Pin(4),rx=Pin(5))
 led = Pin(25, Pin.OUT)
 trigger = Pin(14, Pin.OUT)
 echo = Pin(15, Pin.IN)
 photoic = ADC(2)
-nic_onboard = Pin(22, Pin.OUT)
+nic_onboard = Pin(17, Pin.OUT)
+spi = SPI(1, sck=Pin(10), mosi=Pin(11), miso=Pin(12))
+sd = sdcard.SDCard(spi, Pin(13))
+os.mount(sd, '/sd')
+os.chdir('sd')
+ina = INA219(SHUNT_OHMS, i2c, log_level=INFO)
+ina.configure()
 
 t = Timer()
 mpu = MPU6050()
@@ -22,7 +33,7 @@ longitude = ""
 satellites = ""
 gpsTime = ""
 mode = "rise"
-
+count= 0
 def getPositionData(gps_module):
     global FIX_STATUS, TIMEOUT, latitude, longitude, satellites, gpsTime
     timeout = time.time() + 8   # 8 seconds from now
@@ -65,10 +76,8 @@ def led_light(self):
     utime.sleep(0.1)
 
 def task_gps():
+    global TIMEOUT,gpsTime,latitude,longitude
     getPositionData(gps_module)
-    print("Time:"+str(gpsTime))
-    print("Latitude:"+ str(latitude))
-    print("Longitude:"+str(longitude))
     uart.write(str(gpsTime)+"\t")
     uart.write(str(latitude)+"\t")
     uart.write(str(longitude)+"\n")     
@@ -77,30 +86,63 @@ def task_gps():
         TIMEOUT = False
         uart.write("No data\n")
 
+def solar_calc():
+     print("Current: %.3f mA" % ina.current())
+     print("Power: %.3f mW" % ina.power())
+     uart.write("Current: %.3f mA" % ina.current())
+     uart.write("Power: %.3f mW" % ina.power())
+     utime.sleep(1)
+
+def read_distance():
+    trigger.low()
+    utime.sleep_us(10)
+    trigger.high()
+    utime.sleep(0.00001)
+    trigger.low()
+    while echo.value() == 0:
+        signaloff = utime.ticks_us()
+    while echo.value() == 1:
+        signalon = utime.ticks_us()
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    print("dinstance: ",distance,"cm")
+    
+#t.init(period=3000000,mode=t.PERIODIC,callback=task_gps())
 while True:
+    #t.init(period=1000000,mode=t.PERIODIC,callback=task_gps())
     led_light(led)
     if mode == "rise":
+        count += 1
         val = photoic.read_u16() / 1000
-        uart.write(str(val))
+        uart.write(str(count)+"\n")
+        uart.write("photoic"+str(val)+"\n")
         utime.sleep_ms(2)
+        g=mpu.readData()
+        uart.write("X:{:.2f}  Y:{:.2f}  Z:{:.2f} \n".format(g.Gx,g.Gy,g.Gz))
+        utime.sleep_ms(50)
+        with open("/sd/test.txt", "a") as file:
+            file.write(str(val) + "\r\n")
+        utime.sleep(0.1)
         if val <= 10:
             uart.write("fall")
-            mode = "fall"
+            with open("/sd/test.txt", "a") as file:
+                file.write(str("fall") + "\r\n")
+            utime.sleep(0.1)
+            mode = "land"
     elif mode == "fall" :
         g=mpu.readData()
         uart.write("X:{:.2f}  Y:{:.2f}  Z:{:.2f} \n".format(g.Gx,g.Gy,g.Gz))
         utime.sleep_ms(50)
-        if 1.0 <= g.Gz <= 1.1
-            uart.write("land")
-            mode = "land"
+        with open("/sd/test.txt", "a") as file:
+            file.write("X:{:.2f}  Y:{:.2f}  Z:{:.2f} \n".format(g.Gx,g.Gy,g.Gz)+ "\r\n")
     elif mode == "land" :
+        led_light(led)
         nic_onboard.value(1)
-        utime.sleep(5)
+        utime.sleep(0.4)
         nic_onboard.value(0)
-        utime.sleep(3)
-        nic_onboard.value(1)
-        utime.sleep(5)
-        nic_onboard.value(0)
-        utime.sleep(3)
         uart.write("drive")
         mode = "drive"
+    elif mode == "drive":
+        solar_calc()
+        read_distance()
+    
